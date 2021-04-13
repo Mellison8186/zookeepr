f75a12c65cd7aa6c0b2360d221131ed5607cf7f4
maribels_are_ringing <maribel.duron86@gmail.com>
2021-04-12 19:33:49 -0700

    added the fetch requests and functions to add animals

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const { request } = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -108,6 +110,22 @@ app.get('/api/animals', (req, res) => {
       const animal = createNewAnimal(req.body, animals);
       res.json(animal);
     }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
 app.listen(PORT, () => {
